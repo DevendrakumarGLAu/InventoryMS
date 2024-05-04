729531e7f7e090665a23891d4895e237bb9b2496
Devendra Kumar Singh <devendra1997kumar@gmail.com>
2024-05-04 20:01:05 +0530

    create table in user details

--- src/app/user-details/add/add.component.ts
+++ src/app/user-details/add/add.component.ts
@@ -1,62 +1,100 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+// import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AddProductService } from 'src/app/services/add-product.service';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { SnackBarService } from 'src/app/services/snackbar.service';
+import { FormBuilder, FormGroup, Validators, AbstractControl, ValidationErrors, ValidatorFn } from '@angular/forms';
+import { Observable, of } from 'rxjs';
 @Component({
   selector: 'app-add',
   templateUrl: './add.component.html',
   styleUrls: ['./add.component.css']
 })
 export class AddComponent implements OnInit {
-userForm!:FormGroup;
+  userForm!: FormGroup;
+  userId: any;
+  flag!: string;
+  buttonText!:string;
 
-constructor(private fb:FormBuilder,
-            private AddProductService:AddProductService,
-            private router:Router,
-            private snackBar:SnackBarService) {}
+  constructor(private fb: FormBuilder,
+    private AddProductService: AddProductService,
+    private router: Router,
+    private activatedRoute: ActivatedRoute,
+    private snackBar: SnackBarService) { }
 
-ngOnInit(): void {
-  this.InitializeForm();
-}
-InitializeForm() {
-  this.userForm = this.fb.group({
-    name: ['',Validators.required],
-    email: ['',Validators.required],
-    phone: ['',Validators.required],
-    password: ['',Validators.required],
-    confirmPassword: ['',Validators.required],
-    address: ['',Validators.required],
-    role: ['',Validators.required],
-    action: ['',Validators.required],
-  });
-}
-
-resetform() {
-  this.userForm.reset(this.userForm.value);
-}
-onSubmit() {
-  if (this.userForm.valid) {
-    // console.log(this.userForm.value);
-    const { confirmPassword, ...formDataWithoutConfirmPassword } = this.userForm.value;
-    const val= {
-      table_name: 'users_details',
-      action:'insert',
-      id:0,
-      column_data: formDataWithoutConfirmPassword
-      
-    }
-    console.log(val);
-    this.AddProductService.addData_db_operations(val).subscribe((data: any) => {
-      // console.log(data);
-      if(data.status==='success'){
-        this.snackBar.openSnackBarSuccess([data.message]);
-        this.router.navigate(['/admin/user-details']);
-      }
-      else{
-        this.snackBar.openSnackBarError([data.message]);
-      }
-    })
+  ngOnInit(): void {
+    this.getuserData();
+    this.InitializeForm();
   }
-}
+  getuserData() {
+    this.activatedRoute.queryParams.subscribe(params => {
+      this.userId = params['id'];
+      this.flag = params['flag'];
+      this.buttonText = (this.flag === 'edit') ? 'Update' : 'Submit';
+    })
+    if (this.flag == 'edit') {
+      const val = {
+        Table_name: 'users_details',
+        id: this.userId
+      }
+      this.AddProductService.getData_common(val).subscribe((data: any) => {
+        this.userForm.patchValue(data.data[0]);
+      })
+    }
+  }
+
+  InitializeForm() {
+    this.userForm = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      phone: ['', Validators.required],
+      password: ['', [Validators.required, Validators.minLength(5)]],
+      confirmPassword: ['', Validators.required],
+      address: ['', Validators.required],
+      role: ['', Validators.required],
+      action: ['', Validators.required],
+    // }, { 
+    //   validators: this.passwordMatchValidator // Custom validator for matching passwords
+    });
+  }
+
+  passwordMatchValidator(): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors | null => {
+      const password = control.get('password')?.value;
+      const confirmPassword = control.get('confirmPassword')?.value;
+
+      return password === confirmPassword ? null : { passwordMismatch: true };
+    };
+  }
+
+  resetform() {
+    this.userForm.reset(this.userForm.value);
+  }
+  onSubmit() {
+    if (this.userForm.valid) {
+      // console.log(this.userForm.value);
+      const { confirmPassword, ...formDataWithoutConfirmPassword } = this.userForm.value;
+      const val = {
+        table_name: 'users_details',
+        action: 'insert',
+        id: 0,
+        column_data: this.userForm.value
+      }
+      if(this.userId){
+        val['id'] = this.userId
+        val['action'] = 'update'
+      }
+      console.log(val);
+      this.AddProductService.addData_db_operations(val).subscribe((data: any) => {
+        // console.log(data);
+        if (data.status === 'success') {
+          this.snackBar.openSnackBarSuccess([data.message]);
+          this.router.navigate(['/admin/user-details']);
+        }
+        else {
+          this.snackBar.openSnackBarError([data.message]);
+        }
+      })
+    }
+  }
 }
--- src/app/user-details/user-details.component.ts
+++ src/app/user-details/user-details.component.ts
@@ -1,17 +1,54 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+import { AddProductService } from '../services/add-product.service';
+import { SnackBarService } from '../services/snackbar.service';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-user-details',
   templateUrl: './user-details.component.html',
   styleUrls: ['./user-details.component.css']
 })
 export class UserDetailsComponent implements OnInit {
-dataSource: any;
-displayedColumns: string[] = ['name', 'email']; // Add more column names as needed
+userData: any;
+hasData:boolean = false;
+displayedColumns: string[] = ['Sno', 'name', 'email', 'phone','address','role', 'action'];
+dataSource!: MatTableDataSource<any>;
+pageSize = 10;
+pageSizeOptions = [10, 25, 100];
+length = 100;
+@ViewChild(MatPaginator) paginator!: MatPaginator;
+@ViewChild(MatSort, { static: true }) sort!: MatSort;
+  constructor(private AddProductService:AddProductService,
+    private snackbar:SnackBarService,
+    private router:Router) {}
 
-  constructor() {}
+  ngOnInit(): void {
+    this.loadData();
+  }
 
-  ngOnInit(): void {}
+  loadData(){
+    const value = {
+      Table_name: 'users_details',
+    }
+    this.AddProductService.getData_common(value).subscribe(data => {
+      // this.dataSource = data.data;
+      this.userData = data.data;
+      console.log("this userData",this.userData);
+      this.hasData = this.userData.length > 0;
+      // this.productdata = this.productdata.data;
+      this.dataSource = new MatTableDataSource(this.userData);
+      this.dataSource.paginator = this.paginator;
+      this.dataSource.sort = this.sort;
+    })
+
+  }
+  editProduct(id: number) {
+    this.router.navigate(['/selling/add'], {
+      queryParams: { id: id },
+    });
+  }
 
   applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
