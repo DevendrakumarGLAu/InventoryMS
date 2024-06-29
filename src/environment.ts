a6e03c6702246a9aff8af56d752b5319a3746800
Devendra Kumar Singh <116472581+DevendrakumarGLAu@users.noreply.github.com>
2024-06-29 12:16:41 +0530

    Update environment.ts

--- src/environment.ts
+++ src/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: false,
     // apiUrl: 'http://localhost:5000'
-     apiUrl: 'http://0.0.0.0:5000' 
-    // apiUrl:'https://inventoryms-backend.onrender.com:5000'
+    // apiUrl: 'http://0.0.0.0:5000' 
+    apiUrl:'https://inventoryms-backend.onrender.com:5000'
   };
   
