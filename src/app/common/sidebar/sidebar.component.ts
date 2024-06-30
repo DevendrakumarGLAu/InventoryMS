13578af041ccec0fe14ba5110e1e5939f0a00ba6
Devendra Kumar Singh <devendra1997kumar@gmail.com>
2024-06-30 11:24:37 +0530

    add _sredirect file

--- src/app/common/sidebar/sidebar.component.ts
+++ src/app/common/sidebar/sidebar.component.ts
@@ -1,54 +1,59 @@
 import { Component, OnInit } from '@angular/core';
-import {sidebarMenuConfig} from './config'
+import { sidebarMenuConfig } from './config'
 import { AddProductService } from 'src/app/services/add-product.service';
 // import { Router } from '@angular/router';
 import { ActivatedRoute, Router, NavigationEnd } from '@angular/router';
-import { filter } from 'rxjs/operators';
-interface MenuItem {
-  label: string;
-  route: string;
-  icon: string;
-  submenu?: MenuItem[];
-  showSubmenu?: boolean;
-}
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.css']
 })
 export class SidebarComponent implements OnInit {
-  
   // sidebaritems = sidebarMenuConfig;
-  sidebaritems:any = [];
+  sidebaritems: any = [];
+  childMenu:any =[];
   activeMenuId: number | null = null;
+  AccountId: any
   // route!: string;
-  constructor(private AddProductService:AddProductService,
-    private router:Router,
-    private route:ActivatedRoute
-  ){}
+  constructor(private AddProductService: AddProductService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {
+    this.AccountId = localStorage.getItem('AccountId')
+  }
 
   // toggleSubmenu(event: MouseEvent, menuItem: MenuItem) {
   //   event.preventDefault();
   //   menuItem.showSubmenu = !menuItem.showSubmenu;
   // }
-  ngOnInit(): void {
-    const AccountId = localStorage.getItem('AccountId')
-    console.log(AccountId)
-    const value = {
-      id:AccountId
+  async ngOnInit(): Promise<void> {
+    this.AccountId = localStorage.getItem('AccountId');
+    if (this.AccountId) {
+      const value = {
+        id: this.AccountId
+      };
+
+      try {
+        const data = await this.AddProductService.sidebarConfig(value).toPromise();
+        console.log(data);
+        this.sidebaritems = data;
+        
+        
+        // this.getActiveMenuId();
+      } catch (error) {
+        console.error('Error fetching sidebar data:', error);
+        // Handle error if needed
+      }
     }
-    this.AddProductService.sidebarConfig(value).subscribe(data =>{
-      console.log(data)
-      this.sidebaritems = data
-    })
-    this.getActiveMenuId()
   }
-  getActiveMenuId(): void {
-    const queryParams = this.route.snapshot.queryParams;
-    if (queryParams?.['menuid']) {
-      this.activeMenuId = parseInt(atob(queryParams?.['menuid']), 10);
-    } else {
-      this.activeMenuId = null;
-    }
-  }
+
+  // getActiveMenuId(): void {
+  //   const queryParams = this.route.snapshot.queryParams;
+  //   if (queryParams?.['menuid']) {
+  //     this.activeMenuId = parseInt(atob(queryParams?.['menuid']), 10);
+  //   } else {
+  //     this.activeMenuId = null;
+  //   }
+  // }
+
 }
